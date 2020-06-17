25aa07a13bf0c5352a8706bcb12c08a00b52338c
Ryan Awad <amazing_ryan@yahoo.com>
2020-06-17 17:58:02 -0400

    Initial Commit

---
+++ Chatting-Application/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Chatting_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TextInput = new System.Windows.Forms.TextBox();
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            this.ListenButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.IpBox = new System.Windows.Forms.TextBox();
+            this.PortBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextInput
+            // 
+            this.TextInput.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextInput.Location = new System.Drawing.Point(12, 493);
+            this.TextInput.Name = "TextInput";
+            this.TextInput.Size = new System.Drawing.Size(466, 27);
+            this.TextInput.TabIndex = 0;
+            this.TextInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextInput_KeyDown);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.OutputBox.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputBox.Location = new System.Drawing.Point(12, 12);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ReadOnly = true;
+            this.OutputBox.Size = new System.Drawing.Size(466, 460);
+            this.OutputBox.TabIndex = 3;
+            this.OutputBox.Text = "";
+            // 
+            // ListenButton
+            // 
+            this.ListenButton.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListenButton.Location = new System.Drawing.Point(484, 146);
+            this.ListenButton.Name = "ListenButton";
+            this.ListenButton.Size = new System.Drawing.Size(338, 77);
+            this.ListenButton.TabIndex = 4;
+            this.ListenButton.Text = "Listen for a connection";
+            this.ListenButton.UseVisualStyleBackColor = true;
+            this.ListenButton.Click += new System.EventHandler(this.ListenButton_Click);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectButton.Location = new System.Drawing.Point(484, 229);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(338, 77);
+            this.ConnectButton.TabIndex = 5;
+            this.ConnectButton.Text = "Connect to a listening connection";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // IpBox
+            // 
+            this.IpBox.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IpBox.Location = new System.Drawing.Point(484, 55);
+            this.IpBox.Name = "IpBox";
+            this.IpBox.Size = new System.Drawing.Size(338, 21);
+            this.IpBox.TabIndex = 6;
+            this.IpBox.Text = "127.0.0.1";
+            // 
+            // PortBox
+            // 
+            this.PortBox.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PortBox.Location = new System.Drawing.Point(487, 109);
+            this.PortBox.Name = "PortBox";
+            this.PortBox.Size = new System.Drawing.Size(338, 21);
+            this.PortBox.TabIndex = 7;
+            this.PortBox.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(484, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(484, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Enabled = false;
+            this.CloseButton.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.Location = new System.Drawing.Point(484, 312);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(338, 77);
+            this.CloseButton.TabIndex = 10;
+            this.CloseButton.Text = "Close connection";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(834, 536);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PortBox);
+            this.Controls.Add(this.IpBox);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.ListenButton);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.TextInput);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(850, 575);
+            this.MinimumSize = new System.Drawing.Size(850, 575);
+            this.Name = "Form1";
+            this.Text = "Chat App";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextInput;
+        private System.Windows.Forms.RichTextBox OutputBox;
+        private System.Windows.Forms.Button ListenButton;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox IpBox;
+        private System.Windows.Forms.TextBox PortBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
+
---
+++ Chatting-Application/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Net.Security;
+using System.Security;
+
+namespace Chatting_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static IPAddress RHOST = IPAddress.Parse("127.0.0.1");
+        static int RPORT = 0;
+        static bool connected;
+        static TcpListener listener;
+        static TcpClient client;
+        static Thread thread;
+        static Thread thread2;
+        static Thread thread3;
+        // when listener sends 2 connect, connect see ok. rest is bad
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            void Send()
+            {
+                NetworkStream nwStream = client.GetStream();
+
+                char[] buffer = TextInput.Text.ToCharArray();
+                byte[] convert = Encoding.UTF8.GetBytes(TextInput.Text);
+
+                nwStream.Write(convert, 0, buffer.Length);
+                Invoke(new Action(() =>
+                {
+                    OutputBox.Text += "[" + DateTime.Today + "] - " + TextInput.Text + "\n";
+                }));
+            }
+
+            thread2 = new Thread(Send);
+            thread2.Start();
+        }
+
+        private void ListenButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (PortBox.Text.ToString().Trim() == "0")
+                {
+                    MessageBox.Show("Please enter a valid IP Address or Port number!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ListenButton.Enabled = true;
+                    ConnectButton.Enabled = true;
+                }
+                else
+                {
+                    ListenButton.Enabled = false;
+                    ConnectButton.Enabled = false;
+
+                    RHOST = IPAddress.Parse(IpBox.Text);
+                    RPORT = Convert.ToInt32(PortBox.Text);
+
+                    listener = new TcpListener(RHOST, RPORT);
+
+                    listener.Start();
+
+                    OutputBox.Text = "Listening...";
+
+                    void Connect()
+                    {
+                        client = listener.AcceptTcpClient();
+
+                        Invoke(new Action(() =>
+                        {
+                            OutputBox.Text += "\n***[CONNECTED]***\n";
+                            connected = true;
+                            CloseButton.Enabled = true;
+                        }));
+
+                        try
+                        {
+                            while (true)
+                            {
+                                NetworkStream ns = client.GetStream();
+                                byte[] buffer = new byte[client.ReceiveBufferSize];
+
+                                int bytesRead = ns.Read(buffer, 0, client.ReceiveBufferSize);
+
+                                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                                Invoke(new Action(() =>
+                                {
+                                    OutputBox.Text += "[" + DateTime.Now + "] - " + dataReceived + "\n";
+                                }));
+                            }
+                        }
+                        catch (IOException) // connection closed/lost
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                listener.Stop();
+                                thread.Abort();
+                                OutputBox.Text += "\n**[CONNECTION CLOSED]**";
+                                connected = false;
+                                ListenButton.Enabled = true;
+                                ConnectButton.Enabled = true;
+                                CloseButton.Enabled = false;
+                            }));
+                        }
+                    }
+
+
+                    thread = new Thread(Connect);
+                    thread.Start();
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid IP Address or Port number!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ListenButton.Enabled = true;
+                ConnectButton.Enabled = true;
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Please enter a valid IP Address or Port number!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ListenButton.Enabled = true;
+                ConnectButton.Enabled = true;
+            }
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ListenButton.Enabled = false;
+                ConnectButton.Enabled = false;
+
+                RHOST = IPAddress.Parse(IpBox.Text);
+                RPORT = Convert.ToInt32(PortBox.Text);
+
+                try
+                {
+                    OutputBox.Text = "Connecting...";
+                    IPEndPoint serverEP = new IPEndPoint(RHOST, RPORT);
+                    client = new TcpClient();
+                    client.Connect(serverEP);
+                    OutputBox.Text += "\n***[CONNECTED]***\n";
+                    CloseButton.Enabled = true;
+                    connected = true;
+
+                    void Connect2() // SOLUTION TO CONNECTION CLOSING PROBLEM, MAKE THE RECEIVE LOOP IN A DIFFERENT THREAD THAN THE CONNECTION CLOSED EXEPCTION!
+                    {
+                        try
+                        {
+                            while (true)
+                            {
+                                NetworkStream ns = client.GetStream();
+                                byte[] buffer = new byte[client.ReceiveBufferSize];
+
+                                int bytesRead = ns.Read(buffer, 0, client.ReceiveBufferSize);
+
+                                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                                Invoke(new Action(() =>
+                                {
+                                    OutputBox.Text += "[" + DateTime.Today + "] - " + dataReceived + "\n";
+                                }));
+                            }
+                        }
+                        catch (IOException) // connection closed/lost
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                client.GetStream().Close();
+                                client.Close();
+                                thread3.Abort();
+                                OutputBox.Text += "\n**[CONNECTION CLOSED]**";
+                                connected = false;
+                                ListenButton.Enabled = true;
+                                ConnectButton.Enabled = true;
+                                CloseButton.Enabled = false;
+                            }));
+                        }
+                    }
+
+                    thread3 = new Thread(Connect2);
+                    thread3.Start();
+                }
+                catch (SocketException)
+                {
+                    OutputBox.Text += "\n**[NO CONNECTION]**";
+                    connected = false;
+                    ListenButton.Enabled = true;
+                    ConnectButton.Enabled = true;
+                    CloseButton.Enabled = false;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid IP Address and Port number!", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                ListenButton.Enabled = true;
+                ConnectButton.Enabled = true;
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Please enter a valid IP Address or Port number!", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                ListenButton.Enabled = true;
+                ConnectButton.Enabled = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+            thread.Abort();
+            thread2.Abort();
+            thread3.Abort();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            client.Close();
+            
+            connected = false;
+            ListenButton.Enabled = true;
+            ConnectButton.Enabled = true;
+            CloseButton.Enabled = false;
+        }
+
+        private void TextInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && connected == true)
+            {
+                void Send()
+                {
+                    NetworkStream nwStream = client.GetStream();
+
+                    char[] buffer = TextInput.Text.ToCharArray();
+                    byte[] convert = Encoding.UTF8.GetBytes(TextInput.Text);
+
+                    nwStream.Write(convert, 0, buffer.Length);
+                    Invoke(new Action(() =>
+                    {
+                        OutputBox.Text += "[" + DateTime.Now + "] - " + TextInput.Text + "\n";
+                        TextInput.Text = null;
+                    }));
+                }
+
+                thread2 = new Thread(Send);
+                thread2.Start();
+            }
+        }
+    }
+}
